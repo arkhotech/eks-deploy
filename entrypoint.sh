d44da8730966151d5346b50bd2a0fd4a0cf32a1f
Marcelo Silva <msilva@arkho.io>
2023-04-14 23:34:32 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "Iniciando despliegue usando HELM"
 
+env
+
 export AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID
 
 export AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_ACCESS_KEY
@@ -10,7 +12,7 @@ export AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_ACCESS_KEY
 export AWS_REGION=$INPUT_AWS_REGION
 
 echo "Obteniendo configuración"
-aws eks update-kubeconfig --region $INPUT_AWS_REGION --name $INPUT_CLUSTER_NAME
+aws eks update-kubeconfig --region $AWS_REGION --name $INPUT_CLUSTER_NAME
 
 ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 
