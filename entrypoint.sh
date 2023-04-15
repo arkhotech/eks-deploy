321e3368819f4a6643d1f1841f46c6f832fcc4a9
Marcelo Silva <msilva@arkho.io>
2023-04-14 23:51:09 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,11 @@ echo "Iniciando despliegue usando HELM"
 
 env
 
-export AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID
-
-export AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_ACCESS_KEY
-
-export AWS_REGION=$INPUT_REGION
-
 echo "Obteniendo configuración"
-aws eks update-kubeconfig --region $INPUT_REGION --name $INPUT_CLUSTER_NAME
+aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name $INPUT_CLUSTER_NAME
 
 ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 
-IMAGE="${ACCOUNT_ID}.dkr.ecr.${INPUT_AWS_REGION}.amazonaws.com/${INPUT_SERVICE_NAME}"
+IMAGE="${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${INPUT_SERVICE_NAME}"
 helm upgrade --install tpl-service tpl-service --set image.repository=$IMAGE --set image.tag=$INPUT_SERVICE_VERSION --debug
 
