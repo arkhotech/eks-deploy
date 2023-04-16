1c4f2adf9f93a762fa026326288076cdea83a483
Marcelo Silva <msilva@arkho.io>
2023-04-16 19:17:36 -0400

    correccion

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ echo "Obteniendo configuración"
 
 ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 
+aws eks update-kubeconfig --region ${AWS_DEFAULT_REGION} --name ${INPUT_CLUSTER_NAME}
+
 IMAGE="${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${INPUT_SERVICE_NAME}"
 helm upgrade --install tpl-service tpl-service --set image.repository=$IMAGE --set image.tag=$INPUT_SERVICE_VERSION --debug
 
