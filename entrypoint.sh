f42cacb9b7753f7b1a7377f3cdc113271f8c7eb2
Marcelo Silva <msilva@arkho.io>
2023-04-16 20:33:35 -0400

    Correccion version del container

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,10 @@ echo "Iniciando despliegue usando HELM"
 
 env
 
-echo "Obteniendo configuración"
-
-
 ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 
 aws eks update-kubeconfig --region ${AWS_DEFAULT_REGION} --name ${INPUT_CLUSTER_NAME}
 
-IMAGE="${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${INPUT_SERVICE_NAME}"
+IMAGE="${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${INPUT_SERVICE_NAME}:${INPUT_SERVICE_VERSION}"
 helm upgrade --install ${INPUT_SERVICE_NAME} tpl-service --set image.repository=$IMAGE --set image.tag=$INPUT_SERVICE_VERSION --debug
 
