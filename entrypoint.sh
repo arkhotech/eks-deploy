111cf7710dc69348ef160876fa6a1335324449f5
Marcelo Silva <msilva@arkho.io>
2023-04-17 11:28:36 -0400

    Correccion en chart

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,5 @@ ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 aws eks update-kubeconfig --region ${AWS_DEFAULT_REGION} --name ${INPUT_CLUSTER_NAME}
 
 IMAGE="${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${INPUT_SERVICE_NAME}:${INPUT_SERVICE_VERSION}"
-helm upgrade --install ${INPUT_SERVICE_NAME} ${INPUT_TEMPLATE_PATH} --set image.repository=$IMAGE --set image.tag=$INPUT_SERVICE_VERSION --debug
+helm upgrade --install ${INPUT_SERVICE_NAME} ./${INPUT_TEMPLATE_PATH} --set image.repository=$IMAGE --set image.tag=$INPUT_SERVICE_VERSION --debug
 
