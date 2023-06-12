1d719ea21195c93e8607dc9bde440ded6cf4cb2d
Marcelo Silva <msilva@arkho.io>
2023-06-12 15:32:25 -0400

    Correccion

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "Iniciando despliegue usando HELM"
+
+ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+
+aws eks update-kubeconfig --region ${AWS_DEFAULT_REGION} --name ${INPUT_CLUSTER_NAME}
 
 if [ ! -z "${INPUT_PRE_COMMAND}" ];
 then
     echo "Ejecutando comando pre execution"
     eval "${INPUT_PRE_COMMAND}"
 fi
-
-echo "Iniciando despliegue usando HELM"
-
-ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
-
-aws eks update-kubeconfig --region ${AWS_DEFAULT_REGION} --name ${INPUT_CLUSTER_NAME}
 
 IMAGE="${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${INPUT_SERVICE_NAME}:${INPUT_SERVICE_VERSION}"
 helm upgrade --install ${INPUT_SERVICE_NAME} ./${INPUT_TEMPLATE_PATH} --set image.repository=$IMAGE --set image.tag=$INPUT_SERVICE_VERSION --debug
